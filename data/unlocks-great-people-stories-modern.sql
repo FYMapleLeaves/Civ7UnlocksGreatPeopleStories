263f6468ad614a71b7cbcc81ca07a1987843291f
FYMapleLeaves <1069533792@qq.com>
2025-02-22 23:36:14 +0800

    Upload for the first time
    Upload for the first time.

---
+++ data/unlocks-great-people-stories-modern.sql
@@ -0,0 +1,6 @@
+
+-- UPDATE GreatPersonClasses SET CityStatesSuzerained=0, ConstructibleType='BUILDING_PALACE' WHERE GreatPersonClassType='GREAT_PERSON_CLASS_UPARAT';
+
+-- UPDATE Unit_Costs SET Cost = 10 WHERE UnitType = 'UNIT_UPARAT';
+-- UPDATE Unit_Costs SET Cost = 10 WHERE UnitType LIKE 'UNIT_UPARAT_%';
+-- UPDATE Units SET CostProgressionParam1 = 0 WHERE UnitType='UNIT_UPARAT';
---
+++ data/unlocks-great-people-stories.sql
@@ -0,0 +1,5 @@
+
+
+UPDATE NarrativeStory_Queues SET ActivationCount = 10 WHERE QueueType LIKE 'NARRATIVE_GROUP_GREAT_PERSON_%';
+
+
